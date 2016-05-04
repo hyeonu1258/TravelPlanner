bed4ca7391567b0338f6bdaec4002bb8264770ef
jingyu <jingyu91@naver.com>
2016-05-04 18:46:14 +0900

    네비

--- app/src/main/java/inu/travel/Adapter/NaviListAdapter.java
+++ app/src/main/java/inu/travel/Adapter/NaviListAdapter.java
@@ -8,10 +8,9 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.util.ArrayList;
 
+import inu.travel.Model.NaviDescript;
 import inu.travel.R;
 import inu.travel.ViewHolder.NaviListViewHolder;
 
@@ -19,22 +18,24 @@ import inu.travel.ViewHolder.NaviListViewHolder;
  * Created by jingyu on 2016-04-12.
  */
 public class NaviListAdapter extends BaseAdapter {
-    private NodeList nodeList;
+    private ArrayList<NaviDescript> itemDatas;
     LayoutInflater layoutInflater;
 
-    public NaviListAdapter(NodeList nList, Context context) {
-        this.nodeList = nList;
+    public NaviListAdapter(ArrayList<NaviDescript> items, Context context) {
+        this.itemDatas = items;
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return itemDatas != null ? itemDatas.size() : 0;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+
+        return (itemDatas != null && (itemDatas.size() > position && position >= 0) ? itemDatas.get(position) : null);
+
     }
 
     @Override
@@ -43,36 +44,22 @@ public class NaviListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+    //public View getView(int position, View convertView, ViewGroup parent) {
+
         NaviListViewHolder naviListViewHolder = new NaviListViewHolder();
-        if(convertView == null){
+
+        if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.list_item_navi, parent, false);
-            naviListViewHolder.imgNaviMaker = (ImageView)convertView.findViewById(R.id.imgNaviMarker);
-            naviListViewHolder.txtNaviDescription = (TextView)convertView.findViewById(R.id.txtNaviDesc);
+            naviListViewHolder.imgNaviMaker = (ImageView) convertView.findViewById(R.id.imgNaviMarker);
+            naviListViewHolder.txtNaviDescription = (TextView) convertView.findViewById(R.id.txtNaviDesc);
             convertView.setTag(naviListViewHolder);
-        }else{
-            naviListViewHolder = (NaviListViewHolder) convertView.getTag();
+        } else {
+            naviListViewHolder = (NaviListViewHolder)convertView.getTag();
         }
 
-        Node nNode = nodeList.item(position);
-        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-            Element eElement = (Element) nNode;
-            try { //항목이 null인 것도 있음
-                System.out.println("name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
-                System.out.println("tmap:pointIndex : " + eElement.getElementsByTagName("tmap:pointIndex").item(0).getTextContent());
-                System.out.println("description : " + eElement.getElementsByTagName("description").item(0).getTextContent());
-                naviListViewHolder.txtNaviDescription.setText(eElement.getElementsByTagName("description").item(0).getTextContent());
-                System.out.println("styleUrl : " + eElement.getElementsByTagName("styleUrl").item(0).getTextContent());
-                System.out.println("tmap:nextRoadName : " + eElement.getElementsByTagName("tmap:nextRoadName").item(0).getTextContent());
-                System.out.println("tmap:nodeType : " + eElement.getElementsByTagName("tmap:nodeType").item(0).getTextContent());
-                System.out.println("tmap:turnType : " + eElement.getElementsByTagName("tmap:turnType").item(0).getTextContent());
-                System.out.println("tmap:pointType : " + eElement.getElementsByTagName("tmap:pointType").item(0).getTextContent());
-//                    System.out.println("coordinates : " + eElement.getElementsByTagName("coordinates").item(0).getTextContent());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println("----------------------------");
-        }
+        naviListViewHolder.txtNaviDescription.setText(itemDatas.get(position).getDescription());
+
         return convertView;
     }
 }
---
+++ app/src/main/java/inu/travel/Model/NaviDescript.java
@@ -0,0 +1,16 @@
+package inu.travel.Model;
+
+/**
+ * Created by jingyu on 16. 4. 28..
+ */
+public class NaviDescript {
+    String Description;
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getDescription(){
+        return Description;
+    }
+}
